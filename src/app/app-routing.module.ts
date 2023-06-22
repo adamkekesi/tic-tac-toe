015d02baf41fa2c068a88661c6e853479839ebfa
adamkekesi <kekesi.adam2003@gmail.com>
2023-06-22 09:55:12 +0200

    Scaffolding game page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamePage } from './game';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    component: GamePage,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/game/game.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './game.page.html',
+  styleUrls: ['./game.page.scss'],
+})
+export class GamePage {}
